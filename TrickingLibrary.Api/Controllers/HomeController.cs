5cf3ddbdf802256c1086f263a3a24edd363f3f31
Emmanuel Yirenkyi <yirenemma@gmail.com>
2021-09-03 19:47:06 +0000

    initial commit

---
+++ TrickingLibrary.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string GetItem()
+        {
+            return "Hello  World";
+        }
+    }
+}
---
+++ web-client/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+
+  },
+  extends: [
+
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
+}
